7bb7c0b723fec7e6e68f6495ab69be5ddb7f6265
Wojciech Wernicki <wernickiwojtek@gmail.com>
2020-11-03 17:28:25 +0100

    Add NProgress to indicate loading state for SSR pages

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,17 +1,41 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import PropTypes from 'prop-types';
+import NProgress from 'nprogress';
 
 import Nav from '../src/components/Nav/Nav';
 import Footer from '../src/components/Footer/Footer';
 
 import '../styles/tailwind.css';
 import '../styles/globals.css';
+import 'nprogress/nprogress.css';
 
 function MyApp({ Component, pageProps }) {
   const { locale } = pageProps;
+  const router = useRouter();
+
+  NProgress.configure({ minimum: 0.1, parent: '#wrapper' });
+
+  const startProgress = () => {
+    NProgress.start();
+  };
+
+  const finishProgress = () => {
+    NProgress.done();
+  };
+
+  useEffect(() => {
+    router.events.on('routeChangeStart', startProgress);
+    router.events.on('routeChangeComplete', finishProgress);
+
+    return () => {
+      router.events.off('routeChangeComplete', finishProgress);
+      router.events.off('routeChangeStart', startProgress);
+    };
+  });
 
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col" id="wrapper">
       <Nav locale={locale} />
       <main className="flex flex-grow">
         <Component {...pageProps} />
